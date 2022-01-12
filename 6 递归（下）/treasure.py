c09656047b27b98694eccf89eea79096be1ed2b5
doer <499361328@qq.com>
2022-01-12 18:54:40 +0800

    例行备份

--- 6 递归（下）/treasure.py
+++ 6 递归（下）/treasure.py
@@ -19,6 +19,8 @@ values = {(i, w): 0 for i in range(len(treasures)) for w in range(max_w + 1)}
 # 逐个填写二维表格
 for i in range(1, len(treasures)):
     for w in range(1, max_w + 1):
+        # ps: 同样只能在前i件宝物中选择0~i件宝物，负重越大，能装载的价值肯定越大
+        # 也就是说如果w>0，(i, w) >= (i, (0 ~ w-1))，所以不装第i个宝物时，values[(i, w)] = values[(i-1, w)]
         # 装不下第i个宝物
         if treasures[i]['w'] > w:
             # 不装第i个宝物
