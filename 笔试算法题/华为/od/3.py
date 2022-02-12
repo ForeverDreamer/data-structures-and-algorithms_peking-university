02115a457e4d6e62d422dca5f520d42d37f6ddb8
doer <499361328@qq.com>
2022-02-12 23:59:57 +0800

    华为od笔试题备份

--- 笔试算法题/华为/od/3.py
+++ 笔试算法题/华为/od/3.py
@@ -24,22 +24,23 @@ class BinTree:
         self.data = [0] + data
         self.min_num = min([n for n in self.data if n > 0])
 
-    def dsf(self, i, out):
+    def dfs(self, i, out):
         if self.data[i] == -1:
             return False
         out.append(self.data[i])
+        # 最后两个节点，二选一
         if i*2+1 > len(self.data)-1:
             if out[-1] != self.min_num:
                 out.pop()
                 return False
             return True
-        for j in (i * 2, i * 2 + 1):
-            if self.dsf(j, out):
+        for j in (i*2, i*2+1):
+            if self.dfs(j, out):
                 return True
         out.pop()
         return False
 
 
 out_put = []
-BinTree(input_seq).dsf(1, out_put)
+BinTree(input_seq).dfs(1, out_put)
 print(' '.join([str(n) for n in out_put]))
