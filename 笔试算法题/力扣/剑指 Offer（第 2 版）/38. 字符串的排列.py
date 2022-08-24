d2fcf16c9516a156260b5621f87ea439528703bb
doer <499361328@qq.com>
2022-08-24 18:28:59 +0800

    力扣剑指offer

--- 笔试算法题/力扣/剑指 Offer（第 2 版）/38. 字符串的排列.py
+++ 笔试算法题/力扣/剑指 Offer（第 2 版）/38. 字符串的排列.py
@@ -15,19 +15,21 @@
 
 class Solution:
     def permutation(self, s: str):
-        c, res = list(s), []
-        def dfs(x):
-            if x == len(c) - 1:
-                res.append(''.join(c))   # 添加排列方案
+        all_chars, res = list(s), []
+        def dfs(pos):
+            if pos == len(all_chars) - 1:
+                res.append(''.join(all_chars))   # 添加排列方案
                 return
-            dic = set()
-            for i in range(x, len(c)):
-                if c[i] in dic:
+            pos_chars = set()
+            for i in range(pos, len(all_chars)):
+                if all_chars[i] in pos_chars:
                     continue  # 重复，因此剪枝
-                dic.add(c[i])
-                c[i], c[x] = c[x], c[i]  # 交换，将 c[i] 固定在第 x 位
-                dfs(x + 1)               # 开启固定第 x + 1 位字符
-                c[i], c[x] = c[x], c[i]  # 恢复交换
+                pos_chars.add(all_chars[i])
+                if i != pos:
+                    all_chars[i], all_chars[pos] = all_chars[pos], all_chars[i]  # 交换，将 c[i] 固定在第 x 位
+                dfs(pos+1)               # 开启固定第 x + 1 位字符
+                if i != pos:
+                    all_chars[i], all_chars[pos] = all_chars[pos], all_chars[i]  # 恢复交换
         dfs(0)
         return res
 
